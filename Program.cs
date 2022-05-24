abcfed343b8b59fe7cd731ec0e3282b39458df1e
hrishikesh kamble <kamblehrishi13@gmail.com>
2022-05-24 15:56:24 +0530

    UC2 branch push

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 ﻿Console.WriteLine("Welcome Employee Wage Computation");
 
-
 //Constants
 int IS_FULL_TIME = 1;
+int Emp_RATE_PER_TIME = 20;
+//Variables
+int empHrs = 0;
+int empwage = 0;
 //It is using to generate random numbers
 Random random = new Random();
 //Computation
@@ -10,8 +13,12 @@ int empCheck = random.Next(0, 2);
 if (empCheck == IS_FULL_TIME)
 {
     Console.WriteLine("Employee is Present");
+    empHrs = 8;
 }
 else
 {
     Console.WriteLine("Employee is Absent");
+    empHrs = 0;
 }
+empwage = empHrs * Emp_RATE_PER_TIME;
+Console.WriteLine("EmpWage : " + empwage);
