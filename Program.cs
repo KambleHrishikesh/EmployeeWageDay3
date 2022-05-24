2bd5d78ce51fc50fc447e9fd2f399cf268fa1f55
hrishikesh kamble <kamblehrishi13@gmail.com>
2022-05-24 16:06:14 +0530

    UC3 push

--- Program.cs
+++ Program.cs
@@ -1,1 +1,32 @@
-﻿Console.WriteLine("Welcome Employee Wage Computation");
+﻿
+//Constants
+int IS_PART_TIME = 1;
+int IS_FULL_TIME = 2;
+int Emp_RATE_PER_HOUR = 20;
+
+//Variables
+int empHrs = 0;
+int empwage = 0;
+//It is using to generate random numbers
+Random random = new Random();
+//Computation
+int empCheck = random.Next(0, 2);
+if (empCheck == IS_PART_TIME)
+{
+    Console.WriteLine("Employee is Present part time");
+    empHrs = 4;
+}
+else if (empCheck == IS_FULL_TIME)
+
+{
+    Console.WriteLine("Employee is Present");
+    empHrs = 8;
+}
+else
+{
+    Console.WriteLine("Employee is Absent");
+    empHrs = 0;
+}
+empwage = empHrs * Emp_RATE_PER_HOUR;
+
+Console.WriteLine("EmpWage : " + empwage);
